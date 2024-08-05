76b9b62dd294a445037738e84978ee4eb1991826
Solomon <andywlt78@gmail.com>
2024-08-05 05:24:07 +0100

    u

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -21,6 +21,7 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) => {
           setState(() {
