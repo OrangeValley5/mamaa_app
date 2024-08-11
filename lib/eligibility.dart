7156a40b1aa336dd5cb497f9a5fe0f74b71fe02b
Solomon <andywlt78@gmail.com>
2024-08-11 05:19:33 +0100

    u

--- lib/eligibility.dart
+++ lib/eligibility.dart
@@ -10,6 +10,25 @@ class Eligibility extends StatefulWidget {
 }
 
 class _EligibilityState extends State<Eligibility> {
+  int _amount = 90000; // Initial value of the amount
+
+  // Function to decrease the amount
+  void _decreaseAmount() {
+    setState(() {
+      if (_amount > 10000) {
+        _amount -= 10000;
+      }
+    });
+  }
+
+  void _increaseAmount() {
+    setState(() {
+      if (_amount < 90000) {
+        _amount += 10000;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,26 +115,31 @@ class _EligibilityState extends State<Eligibility> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        border: Border.all(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          width: 2,
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 8,
-                            height: 2.0,
-                            color: Colors.white,
+                    GestureDetector(
+                      onTap: () {
+                        _decreaseAmount();
+                      },
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            width: 2,
                           ),
-                        ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 8,
+                              height: 2.0,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     const SizedBox(
@@ -132,9 +156,9 @@ class _EligibilityState extends State<Eligibility> {
                         const SizedBox(
                           width: 4,
                         ),
-                        const Text(
-                          '90,000',
-                          style: TextStyle(
+                        Text(
+                          '$_amount',
+                          style: const TextStyle(
                               fontWeight: FontWeight.w400,
                               fontSize: 24,
                               color: Color.fromARGB(255, 255, 255, 255)),
@@ -144,26 +168,31 @@ class _EligibilityState extends State<Eligibility> {
                     const SizedBox(
                       width: 10,
                     ),
-                    Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        border: Border.all(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          width: 2,
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.add,
+                    GestureDetector(
+                      onTap: () {
+                        _increaseAmount();
+                      },
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(
                             color: Color.fromARGB(255, 255, 255, 255),
-                            size: 16,
+                            width: 2,
                           ),
-                        ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              Icons.add,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              size: 16,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
@@ -196,8 +225,8 @@ class _EligibilityState extends State<Eligibility> {
                         Container(
                           height: 12,
                           width: 1,
-                          decoration: const BoxDecoration(
-                              color: Color.fromARGB(255, 144, 136, 255)),
+                          decoration:
+                              const BoxDecoration(color: Color(0xFF9088FF)),
                         ),
                         const SizedBox(
                           width: 4,
@@ -255,7 +284,193 @@ class _EligibilityState extends State<Eligibility> {
                     fontWeight: FontWeight.w300),
               ),
               const SizedBox(
-                height: 10,
+                height: 14,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: const Color(0xFF2E38FF),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '91 Days',
+                          style: TextStyle(fontSize: 10, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: const Color(0xFF2E38FF),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '120 Days',
+                          style: TextStyle(fontSize: 10, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                        top: 10, bottom: 10, left: 20, right: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: const Color(0xFF2E38FF),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '180 Days',
+                          style: TextStyle(fontSize: 10, color: Colors.white),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 239, 239, 239),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Reference No',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                              Text(
+                                'ZX43BH8S',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 164, 164, 164),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Payback Amount',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                              Text(
+                                'No loans',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 164, 164, 164),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Duration',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                              Text(
+                                '-',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 164, 164, 164),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Date',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ),
+                              Text(
+                                '-',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color.fromARGB(255, 164, 164, 164),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: Container(
+                      padding: const EdgeInsets.all(15),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xFF2E38FF),
+                      ),
+                      height: 50,
+                      child: const Center(
+                        child: Text(
+                          "Submit",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )),
+                ),
               ),
             ],
           ),
