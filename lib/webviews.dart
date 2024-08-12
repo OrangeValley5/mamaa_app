21d8b202a21d8c2588e9ddb145001c3a7e3514f6
Solomon <andywlt78@gmail.com>
2024-08-12 21:51:19 +0100

    u

--- lib/webviews.dart
+++ lib/webviews.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewScreen extends StatelessWidget {
+class WebViewScreen extends StatefulWidget {
   final String url;
 
   const WebViewScreen({Key? key, required this.url}) : super(key: key);
+
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late WebViewController _controller;
+  bool isLoading = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Web View'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              _controller.reload();
+            },
+          ),
+        ],
       ),
-      body: WebView(
-        initialUrl: url,
-        javascriptMode: JavascriptMode.unrestricted,
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: widget.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController controller) {
+              _controller = controller;
+            },
+            onPageStarted: (String url) {
+              setState(() {
+                isLoading = true;
+              });
+            },
+            onPageFinished: (String url) {
+              setState(() {
+                isLoading = false;
+              });
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.example.com')) {
+                return NavigationDecision.navigate;
+              } else {
+                // Prevent navigation to other sites
+                return NavigationDecision.prevent;
+              }
+            },
+          ),
+          isLoading ? Center(child: CircularProgressIndicator()) : Container(),
+        ],
       ),
     );
   }
